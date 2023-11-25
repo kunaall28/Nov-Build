95f484ca7b02fb40d79d034c53b6181fa39a838f
kunaall28 <131564751+kunaall28@users.noreply.github.com>
2023-11-25 04:30:31 +0000

    Nov-Build

--- index.js
+++ index.js
@@ -49,7 +49,6 @@ const taskContents = document.querySelector(".task__contents");
 const htmlTaskContent = ({ id, title, description, type, url }) => `
   <div class="col-md-6 col-lg-4 mt-3" id=${id} key=${id}>
     <div class='card shadow-sm task__card'>
-    
       <div class='card-header d-flex justify-content-end task__card__header'>
           <button type='button' class='btn btn-outline-primary mr-1.5' name=${id}>
               <i class='fas fa-pencil-alt name=${id}'></i>
